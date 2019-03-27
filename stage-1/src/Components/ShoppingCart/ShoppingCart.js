3a74fb66d31dc941180fe8c681fb2612c213a293
= <=>
2019-03-27 13:36:11 -0700

    squashed all the bugs.

--- stage-1/src/Components/ShoppingCart/ShoppingCart.js
+++ stage-1/src/Components/ShoppingCart/ShoppingCart.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 import "./ShoppingCart.css";
 
 class ShoppingCart extends Component {
+  constructor(props) { 
+    super(props)
+  }
+  
   render() {
+    console.log(this.props.cart)
     let shoppingCartDisplay = this.props.cart.map((element, index) => {
       return (
         <div className="shopping-cart-product-container" key={index}>
@@ -13,7 +18,7 @@ class ShoppingCart extends Component {
             <div className="shopping-cart-button-container">
               <button
                 className="shopping-cart-button"
-                onClick={() => this.props.removeFromCart(element)}
+                onClick={() => this.props.removeFromCart(index)}
               >
                 Remove From Shopping Cart
               </button>
